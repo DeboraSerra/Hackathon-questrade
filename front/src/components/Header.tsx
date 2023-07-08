78f9f33c32c7a8f3eddd2d5d66c13cc23781faca
Debora Serra <debora.r.serra@gmail.com>
2023-07-08 16:10:02 -0300

    feat: add application context

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,16 +1,26 @@
+"use client";
 import logo from "@/assets/Questrade-Logo-Primary-Black-Medium.svg";
+import { context } from "@/lib/context";
 import Image from "next/image";
+import { useContext } from "react";
+import { AiOutlineUser } from "react-icons/ai";
 
 const Header = () => {
+  const { isLogged } = useContext(context);
   return (
-    <div className="flex h-[88px] w-full shadow-sm justify-between">
+    <div className=" h-[88px] w-full  px-20 py-2 shadow-sm">
+      <div className="mx-auto flex h-full max-w-7xl items-center justify-between">
         <Image
           src={logo}
           alt="Questrade"
           width={300}
           height={140}
-          className="max-h-full"
+          className="max-h-full w-auto"
         />
+        <div className="flex h-16 w-16 items-center justify-center rounded-full bg-gray-50">
+          {isLogged ? null : <AiOutlineUser className="text-4xl" />}
+        </div>
+      </div>
     </div>
   );
 };
