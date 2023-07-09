9ecd73e3d7204ada8893dd955a813e9d778d25ca
Diego <diegotimao1104@gmail.com>
2023-07-09 19:05:50 -0300

    tests: Implementando os testes de login no frontend

--- front/src/tests/Login.test.tsx
+++ front/src/tests/Login.test.tsx
@@ -1,5 +1,6 @@
+import { title } from "process"
 import Login from "../components/Login/Login"
-import { render } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 
 jest.mock('next/navigation', () => ({
   useRouter: jest.fn().mockReturnValue({
@@ -17,7 +18,20 @@ jest.mock('next/image', () => ({
 
 describe('Test the login page', () => {
   it('Should match the snapshot', () => {
-    const { container } = render(<Login />)
-    expect(container).toMatchSnapshot()
+    const {container} = render(<Login />);
+    const titleForm = screen.getByText('Login');
+    expect(titleForm).toBeInTheDocument();
+    expect(container).toBeInTheDocument();
+  })
+
+  it('Verifica se existe todas as ', () => {
+    render(<Login />);
+
+    const inputCpf = screen.getByTestId('login-input-cpf');
+    const inputPassword = screen.getByTestId('login-input-password');
+    const buttonSendLogin = screen.getByTestId('login-btn-send');
+    expect(inputCpf).toBeInTheDocument();
+    expect(inputPassword).toBeInTheDocument();
+    expect(buttonSendLogin).toBeInTheDocument();
   })
 })
