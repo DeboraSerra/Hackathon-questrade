9b1bda585cdb1f70bf9d21126230badc647292b3
Debora Serra <debora.r.serra@gmail.com>
2023-07-09 08:50:28 -0300

    feat: add payments list to context

--- front/src/lib/context.tsx
+++ front/src/lib/context.tsx
@@ -1,16 +1,55 @@
 'use client'
 import { ReactNode, createContext, useState } from "react";
 
+interface ListInterface {
+  description: string;
+  dueDate: Date | string;
+  value: number;
+  status: "pending" | "payed" | "expired" | "default";
+}
+
+const mockList: ListInterface[]  = [
+  {
+    description: 'Parcela referente a Maio',
+    dueDate: '10/05/2023',
+    value: 6000,
+    status: 'payed',
+  },
+  {
+    description: 'Parcela referente a Junho',
+    dueDate: '10/06/2023',
+    value: 6000,
+    status: 'expired',
+  },
+  {
+    description: 'Parcela referente a Julho',
+    dueDate: '10/07/2023',
+    value: 6000,
+    status: 'pending',
+  },
+  {
+    description: 'Parcela referente a Agosto',
+    dueDate: '10/08/2023',
+    value: 6000,
+    status: 'default',
+  },
+]
+
+
 export const context = createContext({
   isLogged: false,
-  setIsLogged: (val: boolean) => {}
+  setIsLogged: (val: boolean) => {},
+  paymentList: [] as ListInterface[],
 });
 
 export default function Provider({ children }: { children: ReactNode }) {
   const [isLogged, setIsLogged] = useState(true)
+  const [paymentList, setPaymentList] = useState(mockList)
+  
   const value = {
     isLogged,
     setIsLogged,
+    paymentList,
   };
   return <context.Provider value={value}>{children}</context.Provider>;
 }
