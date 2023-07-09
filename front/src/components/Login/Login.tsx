fa343ced0529877d93c60233f285e10cf9f2130b
Debora Serra <debora.r.serra@gmail.com>
2023-07-09 18:14:41 -0300

    feat: add login

--- front/src/components/Login/Login.tsx
+++ front/src/components/Login/Login.tsx
@@ -42,61 +42,59 @@ const Login = () => {
   }
 
   return (
-    <Suspense fallback={<Spinner />}>
-      <form
-        onSubmit={handleSubmit}
-        className="mt-10 flex w-[400px] flex-col justify-between gap-10 border px-3 py-4 max-sm:mx-3 max-sm:w-full"
+    <form
+      onSubmit={handleSubmit}
+      className="mt-10 flex w-[400px] flex-col justify-between gap-10 border px-3 py-4 max-sm:mx-3 max-sm:w-full"
+    >
+      <h2 className="text-lg font-bold">Login</h2>
+      <label htmlFor="cpf" className="flex flex-col gap-2 text-sm">
+        CPF
+        <input
+          type="text"
+          name="cpf"
+          id="cpf"
+          value={form.cpf}
+          onChange={handleChange}
+          className="rounded-md border-none bg-gray-50"
+        />
+      </label>
+      <label
+        htmlFor="password"
+        className="relative flex flex-col gap-2 text-sm"
       >
-        <h2 className="text-lg font-bold">Login</h2>
-        <label htmlFor="cpf" className="flex flex-col gap-2 text-sm">
-          CPF
-          <input
-            type="text"
-            name="cpf"
-            id="cpf"
-            value={form.cpf}
-            onChange={handleChange}
-            className="rounded-md border-none bg-gray-50"
-          />
-        </label>
-        <label
-          htmlFor="password"
-          className="relative flex flex-col gap-2 text-sm"
-        >
-          Senha
-          <input
-            type={show ? "text" : "password"}
-            name="password"
-            id="password"
-            value={form.password}
-            onChange={handleChange}
-            className="rounded-md border-none bg-gray-50"
-          />
-          <button
-            type="button"
-            onClick={() => setShow(!show)}
-            className="absolute bottom-3 right-2"
-          >
-            {show ? <BsEyeSlash /> : <BsEye />}
-          </button>
-        </label>
+        Senha
+        <input
+          type={show ? "text" : "password"}
+          name="password"
+          id="password"
+          value={form.password}
+          onChange={handleChange}
+          className="rounded-md border-none bg-gray-50"
+        />
         <button
-          type="submit"
-          className="rounded-md bg-green-600 px-6 py-2.5 font-sans text-sm font-semibold text-white"
+          type="button"
+          onClick={() => setShow(!show)}
+          className="absolute bottom-3 right-2"
         >
-          ENTRAR
+          {show ? <BsEyeSlash /> : <BsEye />}
         </button>
-        <p className="text-center font-sans text-xs">
-          Ainda não sou cadastrado.{" "}
-          <a
-            className="cursor-pointer text-blue-100 underline"
-            onClick={() => setRegister(true)}
-          >
-            Fazer cadastro
-          </a>
-        </p>
-      </form>
-    </Suspense>
+      </label>
+      <button
+        type="submit"
+        className="rounded-md bg-green-600 px-6 py-2.5 font-sans text-sm font-semibold text-white"
+      >
+        ENTRAR
+      </button>
+      <p className="text-center font-sans text-xs">
+        Ainda não sou cadastrado.{" "}
+        <a
+          className="cursor-pointer text-blue-100 underline"
+          onClick={() => setRegister(true)}
+        >
+          Fazer cadastro
+        </a>
+      </p>
+    </form>
   );
 };
 
