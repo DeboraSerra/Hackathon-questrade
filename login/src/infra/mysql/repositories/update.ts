4bae236abe1592aac36edc9c406186ce292a9e82
Debora Serra <debora.r.serra@gmail.com>
2023-07-09 19:17:49 -0300

    fix: fix update return

--- login/src/infra/mysql/repositories/update.ts
+++ login/src/infra/mysql/repositories/update.ts
@@ -3,6 +3,7 @@ import { UpdateRepositorie } from "../../../data/repositories/update";
 import { UserRegister, UserUpdate } from "../../../domain/models";
 import { connection } from "../configs/connection";
 import { LoginRepositorie } from "../../../data/repositories/login";
+import { LoginRepositorieAdapter } from "./login";
 
 export class UpdateRepositorieAdapter implements UpdateRepositorie {
   constructor(private loginRepositorie: LoginRepositorie) {}
@@ -38,7 +39,10 @@ export class UpdateRepositorieAdapter implements UpdateRepositorie {
     query += params.join(', ')
     query += ' WHERE cpf = ?'
     values.push(cpf)
-    const user = await connection.execute<ResultSetHeader>(query, values);
+    await connection.execute<ResultSetHeader>(query, values);
+    query = 'SELECT * FROM UserDb.users WHERE cpf = ?';
+    const [[user]] = await connection.execute<RowDataPacket[]>(query, [cpf]);
+    console.log({ user, query, cpf })
     return user as unknown as UserRegister;
   }
 }
