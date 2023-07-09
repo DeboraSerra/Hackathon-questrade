6472bafa03d5c07365f217e28cc820076a3a1c76
Debora Serra <debora.r.serra@gmail.com>
2023-07-09 18:14:35 -0300

    feat: add interfaces and api functions

--- front/src/lib/context.tsx
+++ front/src/lib/context.tsx
@@ -1,5 +1,9 @@
 "use client";
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useEffect, useState } from "react";
+import { LoginPayload, RegisterPayload, UpdateProfilePayload } from "./interfaces";
+import { api } from "./api";
+import { getUser } from "./auth";
+import { parseCpf } from './helpers';
 
 interface ListInterface {
   description: string;
@@ -41,16 +45,19 @@ export const context = createContext({
   paymentList: [] as ListInterface[],
   userInfo: { name: "", email: "", phone: "", cpf: "" },
   paymentInfo: { totalPayed: 0, payedQuotas: 0, totalQuotas: 0, total: 0 },
+  login: (payload: LoginPayload) => {},
+  register: (payload: RegisterPayload) => {},
+  updateProfile: (payload: UpdateProfilePayload) => {},
 });
 
 export default function Provider({ children }: { children: ReactNode }) {
-  const [isLogged, setIsLogged] = useState(true);
+  const [isLogged, setIsLogged] = useState(false);
   const [paymentList, setPaymentList] = useState([]);
   const [userInfo, setUserInfo] = useState({
-    name: "Diego Cavalcanti",
-    email: "diego.cavalcanti@mail.com",
-    phone: "11 98765-4321",
-    cpf: "000.000.000-00",
+    name: "",
+    email: "",
+    phone: "",
+    cpf: "",
   });
   const [paymentInfo, setPaymentInfo] = useState({
     totalPayed: 0,
@@ -59,12 +66,52 @@ export default function Provider({ children }: { children: ReactNode }) {
     total: 0,
   });
 
+  useEffect(() => {
+    saveUserInfo()
+  }, [])
+
+  const saveUserInfo = () => {
+    const user = getUser()
+    if (!user) {
+      setIsLogged(false)
+      return null
+    }
+    setUserInfo({
+      name: user?.name,
+      email: user?.email,
+      phone: user?.phone,
+      cpf: parseCpf(user.cpf),
+    })
+  }
+
+  const login = async (payload: LoginPayload) => {
+    const { data: { token } } = await api.post('/user/login', payload)
+    localStorage.setItem('token', token)
+    setIsLogged(true)
+    saveUserInfo()
+  }
+
+  const register = async (payload: RegisterPayload) => {
+    const { data: { token } } = await api.post('/user/register', payload)
+    localStorage.setItem('token', token)
+    setIsLogged(true)
+    saveUserInfo()
+  }
+
+  const updateProfile = async (payload: UpdateProfilePayload) => {
+    const { cpf, ...user } = payload;
+    await api.put(`/user/${cpf}`, user)
+  }
+
   const value = {
     isLogged,
     setIsLogged,
     paymentList,
     userInfo,
     paymentInfo,
+    login,
+    register,
+    updateProfile,
   };
   return <context.Provider value={value}>{children}</context.Provider>;
 }
