f142cd7216b73e60fc800b44eb063a7233d611af
Debora Serra <debora.r.serra@gmail.com>
2023-07-09 19:28:40 -0300

    fix: add env variable to front on compose

--- login/src/infra/mysql/repositories/update.ts
+++ login/src/infra/mysql/repositories/update.ts
@@ -1,9 +1,8 @@
 import { ResultSetHeader, RowDataPacket } from "mysql2";
+import { LoginRepositorie } from "../../../data/repositories/login";
 import { UpdateRepositorie } from "../../../data/repositories/update";
 import { UserRegister, UserUpdate } from "../../../domain/models";
 import { connection } from "../configs/connection";
-import { LoginRepositorie } from "../../../data/repositories/login";
-import { LoginRepositorieAdapter } from "./login";
 
 export class UpdateRepositorieAdapter implements UpdateRepositorie {
   constructor(private loginRepositorie: LoginRepositorie) {}
@@ -16,33 +15,36 @@ export class UpdateRepositorieAdapter implements UpdateRepositorie {
     password,
     proofOfIncome,
   }: UserUpdate): Promise<UserRegister> {
-
     let query = "UPDATE UserDb.users SET ";
-    const params = []
-    const values = []
+    const params = [];
+    const values = [];
     if (email) {
-      params.push('email = ?')
-      values.push(email)
-    } if (name) {
-      params.push('name = ?')
-      values.push(name)
-    }if (phone) {
-      params.push('phone = ?')
-      values.push(phone)
-    }if (password) {
-      params.push('password = ?')
-      values.push(password)
-    }if (proofOfIncome) {
-      params.push('proofOfIncome = ?')
-      values.push(proofOfIncome)
+      params.push("email = ?");
+      values.push(email);
     }
-    query += params.join(', ')
-    query += ' WHERE cpf = ?'
-    values.push(cpf)
+    if (name) {
+      params.push("name = ?");
+      values.push(name);
+    }
+    if (phone) {
+      params.push("phone = ?");
+      values.push(phone);
+    }
+    if (password) {
+      params.push("password = ?");
+      values.push(password);
+    }
+    if (proofOfIncome) {
+      params.push("proofOfIncome = ?");
+      values.push(proofOfIncome);
+    }
+    query += params.join(", ");
+    query += " WHERE cpf = ?";
+    values.push(cpf);
     await connection.execute<ResultSetHeader>(query, values);
-    query = 'SELECT * FROM UserDb.users WHERE cpf = ?';
+    query = "SELECT * FROM UserDb.users WHERE cpf = ?";
     const [[user]] = await connection.execute<RowDataPacket[]>(query, [cpf]);
-    console.log({ user, query, cpf })
+    console.log({ user, query, cpf });
     return user as unknown as UserRegister;
   }
 }
